ad5ef301de2ccf1ca25e6ee945f00988ba081908
ssupix <151896598+ssupix@users.noreply.github.com>
2024-12-05 12:54:57 -0800

    Output css ignored

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Universe from '../assets/icons/Universe.svg';
 const Header = () => {
     return (
         <header className="flex items-start justify-between px-8 py-4">
-            <div className="flex flex-col gap-0">
+            <div className="flex flex-col">
                 <h1 className="m-0 p-0 uppercase leading-none">Horizons</h1>
                 <h2 className="m-0 p-0 uppercase leading-none">A gallery of our universe</h2>
             </div>
