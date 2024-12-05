ba3211cb6bafc4399c484f9d8913b704cc4c1427
ssupix <151896598+ssupix@users.noreply.github.com>
2024-12-05 12:31:09 -0800

    header element completed
    - Desktop layout
    - Universe icon imported

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="
       m-auto
       max-w-[1920px]
     ">
+      <Header></Header>
 
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Universe from '../assets/icons/Universe.svg';
+
+const Header = () => {
+    return (
+        <header className="flex items-start justify-between px-8 py-4">
+            <div className="flex flex-col gap-0">
+                <h1 className="m-0 p-0 uppercase leading-none">Horizons</h1>
+                <h2 className="m-0 p-0 uppercase leading-none">A gallery of our universe</h2>
+            </div>
+            <div className="transform translate-y-[-6rem] translate-x-[6rem]">
+                <img src={Universe} alt="Universe" className="h-[32rem] animate-rotation" />
+            </div>
+        </header>
+    );
+};
+
+export default Header;
