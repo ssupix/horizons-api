1f6f49c71240ac8abc49a3b2c15d85561a4cdec3
plries <apaulries@gmail.com>
2024-12-05 12:04:31 -0800

    created footer

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+function Footer() {
+    return (
+        <footer className="
+                w-full
+                flex flex-col
+                items-end px-10 py-5
+            ">
+            <h5 className="w-fit text-right">HORIZONS</h5>
+            <h6 className="w-fit text-right">
+                <a href="mailto:jayden.inniss.com">JAYDEN.INNISS@GMAIL.COM</a>
+                <br/>
+                778-793-5323
+            </h6>
+        </footer>
+    );
+}
+
+export default Footer;
