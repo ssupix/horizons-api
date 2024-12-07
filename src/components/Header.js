115c8d436368bb12651d62cca1e57243588f9aed
ssupix <151896598+ssupix@users.noreply.github.com>
2024-12-06 19:37:02 -0800

    header overflow fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Universe from '../assets/icons/Universe.svg';
 
 const Header = () => {
     return (
-        <header className="flex items-start justify-between px-8 py-4">
+        <header className="flex items-start justify-between px-8 py-4 overflow-hidden">
             <div className="flex flex-col">
                 <h1 className="m-0 p-0 uppercase leading-none">Horizons</h1>
                 <h2 className="m-0 p-0 uppercase leading-none">A gallery of our universe</h2>
